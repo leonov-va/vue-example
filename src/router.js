48d66fbd1362717a53fe3ec946b2f1911a16c17e
Vladislav Leonov <leonov.xi@gmail.com>
2022-02-08 23:37:03 +0300

    update version

--- src/router.js
+++ src/router.js
@@ -1,19 +1,12 @@
 import Home from "@/views/Home.vue";
-import Vue from "vue";
-import Router from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 
-Vue.use(Router);
+const routes = [
+  { path: "/", component: Home },
+  { path: "/todos", component: () => import("./views/Todos.vue") },
+];
 
-export default new Router({
-  mode: "history",
-  routes: [
-    {
-      path: "/",
-      component: Home,
-    },
-    {
-      path: "/todos",
-      component: () => import("./views/Todos.vue"),
-    },
-  ],
+export default createRouter({
+  history: createWebHashHistory(),
+  routes,
 });
